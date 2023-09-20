bf755e77226fad1faad4ed289dbc27eadbcc0f73
273744 <139324075+273744@users.noreply.github.com>
2023-09-20 10:11:41 +0530

    Update main1.tf

--- main1.tf
+++ main1.tf
@@ -1,6 +1,12 @@
+provider "google" {
+  credentials = file(var.gcp_credentials_file)
+  project     = var.gcp_project_id
+  region      = var.gcp_region
+}
+
 resource "google_service_account" "my_service_account" {
-  account_id   = "my-service-account"
-  display_name = "My Service Account"
+  account_id   = var.service_account_name
+  display_name = var.service_account_display_name
 }
 
 resource "google_service_account_key" "my_service_account_key" {
