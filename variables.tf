030b3c570b63d282e054275ff7265f1ecdde3d12
273744 <139324075+273744@users.noreply.github.com>
2023-09-20 10:12:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,20 @@
-variable "service_account_id" {
- type = string
+variable "gcp_credentials_file" {
+  description = "Path to the GCP credentials JSON file."
+}
+
+variable "gcp_project_id" {
+  description = "Google Cloud Project ID."
+}
+
+variable "gcp_region" {
+  description = "GCP region to use for resources."
+  default     = "us-central1"
+}
+
+variable "service_account_name" {
+  description = "Name of the service account to create."
+}
+
+variable "service_account_display_name" {
+  description = "Display name of the service account."
 }
